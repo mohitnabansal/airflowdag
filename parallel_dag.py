89265043b0b65c54c948d36578cb4c6e7cf6f50e
Mohit Bansal <mohitnbansal@gmail.com>
2024-12-31 19:11:24 +0530

    Update parallel_dag.py

--- parallel_dag.py
+++ parallel_dag.py
@@ -1,7 +1,36 @@
-dag = DAG('hello_world', description='Hello World DAG',
-          schedule_interval='0 12 * * *',
-          start_date=datetime(2022, 8, 24), catchup=False)
+from airflow import DAG
+from airflow.providers.cncf.kubernetes.operators.kubernetes_pod import KubernetesPodOperator
+from airflow.utils.dates import days_ago
 
-hello_operator = PythonOperator(task_id='hello_task', python_callable=print_hello, dag=dag)
+# Define default arguments
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+}
 
-hello_operator
+# Define the DAG
+with DAG(
+    'kubernetes_operator_dag',
+    default_args=default_args,
+    description='A simple KubernetesPodOperator DAG',
+    schedule_interval='@daily',
+    start_date=days_ago(1),
+    catchup=False,
+) as dag:
+
+    # Define the KubernetesPodOperator task
+    k8s_task = KubernetesPodOperator(
+        namespace='default',
+        image="python:3.8-slim",
+        cmds=["python", "-c"],
+        arguments=["print('Hello from KubernetesPodOperator')"],
+        labels={"foo": "bar"},
+        name="k8s_task",
+        task_id="k8s_task",
+        get_logs=True,
+    )
+
+    k8s_task
