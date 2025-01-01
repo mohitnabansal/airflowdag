d15cf562d243152d61c161c9c18ca4bd77ac69f3
Mohit Bansal <mohitnbansal@gmail.com>
2025-01-01 11:37:56 +0530

    Update hello_world_dag.py

--- hello_world_dag.py
+++ hello_world_dag.py
@@ -21,9 +21,8 @@ with DAG(dag_id="hello_world_dag",
          start_date=datetime(2021,1,1),
          schedule_interval="@hourly",
          catchup=False) as dag:
-
-    # Define the PythonOperator task
-task1 = PythonOperator(
+      
+      task1 = PythonOperator(
         task_id="hello_world",
         python_callable=helloWorld)
 
